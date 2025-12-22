645bf25905efbb74e3e5b057021a221a625fda01
Abdulrahman Zrein <abdulrahmangit123@gmail.com>
2025-12-22 11:47:23 -0500

    Fix header guard and finalize linked list API

---
+++ c_src/ds.h
@@ -0,0 +1,37 @@
+#ifndef DS_H
+#define DS_H
+
+#include <stddef.h>  // for size_t
+
+/* =========================
+   Linked List Structures
+   ========================= */
+
+typedef struct node {
+    int value;
+    struct node *next;
+} node_t;
+
+typedef struct {
+    node_t *head;
+    size_t size;
+} list_t;
+
+/* =========================
+   Linked List API
+   ========================= */
+
+// Create and destroy
+list_t *list_create(void);
+void list_destroy(list_t *list);
+
+// Operations
+void list_push_front(list_t *list, int value);
+void list_push_back(list_t *list, int value);
+int  list_pop_front(list_t *list, int *out_value);
+
+// Accessors (for visualization)
+size_t list_size(const list_t *list);
+node_t *list_head(const list_t *list);
+
+#endif // DS_H
